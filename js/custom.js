b9cae12dfa77637d188b7be71bb1ab83e718a387
Mudit Lal <46276282+Mudit-Lal@users.noreply.github.com>
2020-04-21 16:45:03 +0530

    GST Cont.

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,11 @@
-/* 
+/*
 
 1. Add your custom JavaScript code below
 2. Place the this code in your template:
 
-  
+
 
 */
+    document.getElementById("GST").onclick = function () {
+        location.href = "gst/gst_begin.html";
+    };
