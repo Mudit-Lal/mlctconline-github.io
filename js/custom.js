37d490bf994fd30ed0698d8161d99ff804ea07b4
Mudit Lal <46276282+Mudit-Lal@users.noreply.github.com>
2020-04-21 18:33:58 +0530

    Addeed code to button

--- js/custom.js
+++ js/custom.js
@@ -9,3 +9,24 @@
     document.getElementById("GST").onclick = function () {
         location.href = "gst_begin.html";
     };
+    document.getElementById("IT").onclick = function () {
+        location.href = "page-under-construction.html";
+    };
+    document.getElementById("EPF").onclick = function () {
+        location.href = "page-under-construction.html";
+    };
+    document.getElementById("CG").onclick = function () {
+        location.href = "page-under-construction.html";
+    };
+    document.getElementById("LA").onclick = function () {
+        location.href = "page-under-construction.html";
+    };
+    document.getElementById("FSA").onclick = function () {
+        location.href = "page-under-construction.html";
+    };
+    document.getElementById("ISO").onclick = function () {
+        location.href = "page-under-construction.html";
+    };
+    document.getElementById("CA").onclick = function () {
+        location.href = "page-under-construction.html";
+    };
